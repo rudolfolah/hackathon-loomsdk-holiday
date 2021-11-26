b46e4d514e303fc4acd62b4a75c3aaf88a81680e
Rudolf Olah <rudolf.olah.to@gmail.com>
2021-11-25 22:31:53 -0500

    more styling

--- src/screens/Setup.jsx
+++ src/screens/Setup.jsx
@@ -1,9 +1,9 @@
 import {useState} from "react";
+import {Redirect} from "react-router-dom";
 
 import {createCompanyData} from "../firebase";
 
 import "./Setup.css";
-import {Redirect} from "react-router-dom";
 
 export function Setup({ baseUrl }) {
   const [companyId, setCompanyId] = useState();
@@ -18,10 +18,10 @@ export function Setup({ baseUrl }) {
   return (
     <div className={"Setup--container"}>
       <header>
-        <h2 className="animate__animated animate__rubberBand">Howdy folks! Welcome to Trivia Town!</h2>
+        <h2 className="animate__animated animate__pulse">Howdy folks! Welcome to Trivia Town!</h2>
         <p>Trivia Town has partnered with Loom video recording software to bring you a brand new way to play trivia games with your coworkers and friends!</p>
         <p>What makes this game different? You do! Every person that you invite to the party will have to pre-record one of the special trivia questions we randomly select for you. Keep it classy or really have fun with it - it’s up to you! All the recorded questions are added to your party, Trivia Party! </p>
-        <h3 className="animate__animated animate__shakeY animate__delay-2s">Setting up a New Trivia Town Party is as easy as 1, 2, 3, 4</h3>
+        <h3 className="animate__animated animate__pulse animate__delay-1s">Setting up a New Trivia Town Party is as easy as 1, 2, 3, 4</h3>
       </header>
       <div className="Setup--steps-container">
         <div className="Setup--step">
@@ -64,9 +64,7 @@ export function Setup({ baseUrl }) {
             className={"Setup--button animate__animated animate__heartBeat animate__delay-5s"}
             onClick={handleClick}
           >
-            Start a new Trivia Town Party!
-            <br/>
-            Go to Town!
+            Click Here to Start
           </button>
         </div>
       </div>
