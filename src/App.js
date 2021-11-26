b0c13988ffd811415fa81246b3a6ed0ab1a2f45a
Rudolf Olah <rudolf.olah.to@gmail.com>
2021-11-25 21:52:53 -0500

    more styling

--- src/App.js
+++ src/App.js
@@ -18,33 +18,33 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img height="300" alt="" src={headerImage} />
+        <header className="App--header">
+          <img height="350" alt="" src={headerImage} />
         </header>
         <section className="App--content">
-        <Switch>
-          <Route path="/invite/:companyId">
-            <Screen.Invite />
-          </Route>
-          <Route path="/play/:companyId">
-            <Screen.Play />
-          </Route>
-          <Route path="/scoreboard/:companyId">
-            <Screen.Scoreboard />
-          </Route>
-          <Route path="/setup/:companyId">
-            <Screen.Manage baseUrl={baseUrl} />
-          </Route>
-          <Route path="/">
-            <Screen.Setup baseUrl={baseUrl} />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route path="/invite/:companyId">
+              <Screen.Invite />
+            </Route>
+            <Route path="/play/:companyId">
+              <Screen.Play />
+            </Route>
+            <Route path="/scoreboard/:companyId">
+              <Screen.Scoreboard />
+            </Route>
+            <Route path="/setup/:companyId">
+              <Screen.Manage baseUrl={baseUrl} />
+            </Route>
+            <Route path="/">
+              <Screen.Setup baseUrl={baseUrl} />
+            </Route>
+          </Switch>
         </section>
         <footer className="App--footer">
           <ul className="App--footer-nav">
             <li><a href={"/about"}>About</a></li>
             <li><a href={"/about"}>Contact Us</a></li>
-            <li><a href={"/about"}>Support</a></li>
+            <li><a href={"/about"}>Customer Support</a></li>
           </ul>
         </footer>
       </div>
--- src/screens/Setup.jsx
+++ src/screens/Setup.jsx
@@ -18,7 +18,7 @@ export function Setup({ baseUrl }) {
   return (
     <div className={"Setup--container"}>
       <header>
-        <h2 className="animate__animated animate__rubberBand">Welcome to Trivia Town! Howdy folks!</h2>
+        <h2 className="animate__animated animate__rubberBand">Howdy folks! Welcome to Trivia Town!</h2>
         <p>Trivia Town has partnered with Loom video recording software to bring you a brand new way to play trivia games with your coworkers and friends!</p>
         <p>What makes this game different? You do! Every person that you invite to the party will have to pre-record one of the special trivia questions we randomly select for you. Keep it classy or really have fun with it - it’s up to you! All the recorded questions are added to your party, Trivia Party! </p>
         <h3 className="animate__animated animate__shakeY animate__delay-2s">Setting up a New Trivia Town Party is as easy as 1, 2, 3, 4</h3>
@@ -29,7 +29,7 @@ export function Setup({ baseUrl }) {
             Step 1
           </div>
           <div className="Setup--step-description">
-            <p>Set-up your unique Trivia Party ID and management page.</p>
+            <p className="color__purple font__bold">Set-up your unique Trivia Party ID and management page.</p>
             <p>As a party host you will have access to everything you need to plan your fun-tastic trivia party!</p>
           </div>
         </div>
@@ -38,8 +38,7 @@ export function Setup({ baseUrl }) {
             Step 2
           </div>
           <div className="Setup--step-description">
-            <p>Send out the Record Video Link invite to all the trivia-masterminds you want to join in.</p>
-            <p>The same link goes out to everyone but each question will be different.</p>
+            <p><span className="color__blue font__bold">Send out the Record Video Link invite</span> to all the trivia-masterminds you want to join in.</p>
             <p>We have it all, from science and Hell’s Kitchen to Beyonce and the fear of bubble-wrap!</p>
           </div>
         </div>
@@ -48,7 +47,7 @@ export function Setup({ baseUrl }) {
             Step 3
           </div>
           <div className="Setup--step-description">
-            <p>Once enough of your party folks record their individual videos using Loom, send out the Play Trivia Town Party Link to your friends and colleagues.</p>
+            <p>Once enough of your party folks record their individual videos using Loom, <span className="color__purple font__bold">send out the Play Trivia Town Party Link</span> to your friends and colleagues.</p>
             <p>Next stop? Trivia Town!</p>
           </div>
         </div>
@@ -57,7 +56,7 @@ export function Setup({ baseUrl }) {
             Step 4
           </div>
           <div className="Setup--step-description">
-            <p>Enjoy the sweet taste of Trivia Town success by watching the Party Scoreboard. Now get Looming!</p>
+            <p><span className="color__blue font__bold">Enjoy the sweet taste of Trivia Town</span> success by watching the Party Scoreboard. Now get Looming!</p>
           </div>
         </div>
         <div className={"Setup--cta"}>
